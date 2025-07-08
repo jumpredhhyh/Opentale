4a66687d64c735939a76b1f71a477a737cd70779
Schoggi <matt.bossi@bluewin.ch>
2025-07-08 23:25:48 +0200

    Fix player state not registered

--- src/player/player_plugin.rs
+++ src/player/player_plugin.rs
@@ -1,6 +1,7 @@
 use crate::player::player_camera_movement::move_camera;
 use crate::player::player_component::spawn_player;
 use crate::player::player_movement::{move_body, movement};
+use crate::player::player_state::PlayerState;
 use crate::ui::main_menu_state::MainMenuState;
 use bevy::core_pipeline::experimental::taa::TemporalAntiAliasPlugin;
 use bevy::prelude::*;
@@ -9,7 +10,8 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(TemporalAntiAliasPlugin)
+        app.init_state::<PlayerState>()
+            .add_plugins(TemporalAntiAliasPlugin)
             .add_systems(Update, (movement, move_camera, move_body))
             .add_systems(OnEnter(MainMenuState::Hidden), spawn_player);
     }
